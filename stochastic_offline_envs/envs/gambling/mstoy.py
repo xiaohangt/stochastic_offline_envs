fa53ae4e0d3454b94e25f8682aaa9f1df93753a2
afonsosamarques <afonsosamarques@gmail.com>
2023-12-17 20:22:20 +0000

    Changes to new mstoy implementation

--- stochastic_offline_envs/envs/gambling/mstoy.py
+++ stochastic_offline_envs/envs/gambling/mstoy.py
@@ -6,10 +6,10 @@ from gym import spaces
 class MSToyEnv(gym.Env):
 
     def __init__(self):
-        self.action_space = spaces.Discrete(1)
-        self.adv_action_space = spaces.Discrete(1)
+        self.action_space = spaces.Discrete(2)
+        self.adv_action_space = spaces.Discrete(2)
         self.observation_space = spaces.Box(
-            low=0, high=1, shape=(16,), dtype=np.uint8)
+            low=0, high=1, shape=(13,), dtype=np.uint8)
         self.state = 0
 
     def get_obs(self):
@@ -25,61 +25,58 @@ class MSToyEnv(gym.Env):
         reward = 0
         done = False
         adv_action = np.random.choice(2, 1)[0]
-        action = 0 if action < 0.5 else 1
 
         if self.state == 0:
             if action == 0:
-                self.state = 1
+                if adv_action == 0:
+                    self.state = 1
+                elif adv_action == 1:
+                    self.state = 2
+            elif action == 1:
                 if adv_action == 0:
                     self.state = 3
                 elif adv_action == 1:
                     self.state = 4
-            elif action == 1:
-                self.state = 2
+                    reward = 15.0
+                    done = True
+        elif self.state == 1:
+            if action == 0:
                 if adv_action == 0:
                     self.state = 5
+                    reward = -1.0
+                    done = True
                 elif adv_action == 1:
                     self.state = 6
-                    reward = 200
-                    done = True
-        elif self.state == 3:
-            if action == 0:
-                self.state = 7
-                if adv_action == 0:
-                    self.state = 13
-                    reward = -10
-                    done = True
-                elif adv_action == 1:
-                    self.state = 14
-                    reward = 0
+                    reward = 0.0
                     done = True
             elif action == 1:
-                self.state = 8
                 if adv_action == 0:
-                    self.state = 15
-                    reward = -30
+                    self.state = 7
+                    reward = -3.0
                     done = True
                 elif adv_action == 1:
-                    self.state = 16
-                    reward = 40
+                    self.state = 8
+                    reward = 4.0
                     done = True
-        elif self.state == 4:
+        elif self.state == 2:
             if action == 0:
                 self.state = 9
-                reward = -40
+                reward = -2.0
                 done = True
             elif action == 1:
                 self.state = 10
-                reward = 100
+                reward = 10.0
                 done = True
-        elif self.state == 5:
+        elif self.state == 3:
             if action == 0:
                 self.state = 11
-                reward = -50
+                reward = -5.0
                 done = True
             elif action == 1:
                 self.state = 12
-                reward = 100
+                reward = -1.5
                 done = True
+        else:
+            raise RuntimeError("Invalid state")
             
         return self.get_obs(), reward, done, {"adv": adv_action}
